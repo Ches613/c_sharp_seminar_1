3db223fe9864aeeb2b1b0b7a6c707c26c35f16f3
Ches613 <zverkovmi@gmail.com>
2022-08-30 21:17:09 +0300

    task2 is done!

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// =========================================================================
+// Напишите программу которая принимает два числа и выводит является ли
+//  второе число квадратом первого
+// =========================================================================
+
+while (true)
+{
+    Console.WriteLine("Enter first number:");
+    string? inputLine1 = Console.ReadLine();
+
+    if (int.TryParse(inputLine1, out int number1))
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter second number:");
+            string? inputLine2 = Console.ReadLine();
+            if (int.TryParse(inputLine2, out int number2))
+            {
+                Console.WriteLine(Math.Sqrt(number2) == double.Parse(inputLine1));
+                return 0;
+            }
+        }
+    }
+}
