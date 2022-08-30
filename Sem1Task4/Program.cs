30c5eec96321850c330dffa8ace92169d235f542
Ches613 <zverkovmi@gmail.com>
2022-08-30 23:20:53 +0300

    Sem1Task4 is done!

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿// =========================================================================
+// Напишите программу, которая принимает на вход три числа и 
+// выдаёт максимальное из этих чисел.
+// =========================================================================
+
+while (true)
+{
+    Console.WriteLine("Enter first number:");
+    string? inputLine1 = Console.ReadLine();
+    int[] numbers = new int[3];
+
+    if (int.TryParse(inputLine1, out numbers[0]))
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter second number:");
+            string? inputLine2 = Console.ReadLine();
+            if (int.TryParse(inputLine2, out numbers[1]))
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter third number:");
+                    string? inputLine3 = Console.ReadLine();
+                    if (int.TryParse(inputLine3, out numbers[2]))
+                    {
+                        Console.WriteLine(numbers.Max());
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+}
