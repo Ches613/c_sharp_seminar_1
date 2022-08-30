361634d3ba06fc0222d2612f0c6b90fca5fadf6e
Ches613 <zverkovmi@gmail.com>
2022-08-30 20:59:24 +0300

    program is done!

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// =========================================================================
+// Напишите программу которая на вход принимает число и выдает его
+// квадрат.
+// =========================================================================
+
+while (true)
+{
+    Console.WriteLine("Enter number:");
+    string? inputLine = Console.ReadLine();
+    if (int.TryParse(inputLine, out int number))
+    {
+        Console.WriteLine(Math.Pow(number, 2));
+        return;
+    }
+
+}
