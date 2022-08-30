8a65b6f2969cb01a0b7b8b2fc0f459f38cc71179
Ches613 <zverkovmi@gmail.com>
2022-08-30 22:17:37 +0300

    Sem1Task7 is done!

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿// =========================================================================
+// Напишите программу, которая принимает на вход трёхзначное число и на
+// выходе показывает последнюю цифру этого числа.
+// =========================================================================
+
+while (true)
+{
+    Console.WriteLine("Enter three-digit number:");
+    string? inputLineN = Console.ReadLine();
+
+    if (int.TryParse(inputLineN, out int inputNumberN) && inputNumberN < 1000 && inputNumberN > 99)
+    {
+            Console.Write(inputNumberN % 10);
+        return;
+    }
+}
