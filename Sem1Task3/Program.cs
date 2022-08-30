d83c6bad82ee001cced5bc3754902dd05faa4def
Ches613 <zverkovmi@gmail.com>
2022-08-30 21:39:08 +0300

    Sem1Task3 is done!

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// =========================================================================
+// Напишите программу которая принимает число и выводит 
+// Соответствующий день недели
+// =========================================================================
+
+while (true)
+{
+    Console.WriteLine("Enter day number:");
+    string? inputLine = Console.ReadLine();
+
+    if (int.TryParse(inputLine, out int inputDayOfWeek))
+    {
+        string[] dayOfWeek = new string[7];
+        dayOfWeek[0] = "Monday";
+        dayOfWeek[1] = "Tuesday";
+        dayOfWeek[2] = "Wednesday";
+        dayOfWeek[3] = "Thursday";
+        dayOfWeek[4] = "Friday";
+        dayOfWeek[5] = "Saturday";
+        dayOfWeek[6] = "Sunday";
+        if (inputDayOfWeek <= dayOfWeek.Length)
+        {
+            Console.WriteLine(dayOfWeek[inputDayOfWeek - 1]);
+            return;
+        }
+        else
+        {
+            Console.WriteLine("there is no such day of the week!");
+        }
+    }
+}
