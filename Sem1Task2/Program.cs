bed2e9d04bbd441cee46fabd08eee7e731610032
Ches613 <zverkovmi@gmail.com>
2022-08-30 23:09:12 +0300

    Sem1Task2 is done!

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// =========================================================================
+// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// =========================================================================
+
+while (true)
+{
+    Console.WriteLine("Enter first number:");
+    string? inputLine1 = Console.ReadLine();
+
+    if (int.TryParse(inputLine1, out int number1))
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter second number:");
+            string? inputLine2 = Console.ReadLine();
+            if (int.TryParse(inputLine2, out int number2))
+            {
+                if (number1 > number2)
+                {
+                    Console.WriteLine("max = " + number1);
+                }
+                else
+                {
+                    Console.WriteLine("max = " + number2);
+                }
+                return 0;
+            }
+        }
+    }
+}
