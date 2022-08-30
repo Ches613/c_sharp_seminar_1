64d2fd734c6cd0e71643246d39faf1f56a26d99c
Ches613 <zverkovmi@gmail.com>
2022-08-30 23:36:26 +0300

    Sem1Task8 is done!

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,23 @@
+﻿// =========================================================================
+// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// =========================================================================
+
+while (true)
+{
+    Console.WriteLine("Enter number:");
+    string? inputLine = Console.ReadLine();
+
+    if (int.TryParse(inputLine, out int number))
+    {
+        int outputNumber = 2;
+        List<int> outputNumbers = new List<int>();
+        while (outputNumber <= number)
+        {
+            outputNumbers.Add(outputNumber);
+            outputNumber += 2;
+        }
+        Console.WriteLine(string.Join(", ", outputNumbers));
+        return 0;
+    }
+}
