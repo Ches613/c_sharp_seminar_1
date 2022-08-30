13b592adf0a6d1cbf0f093941bdb0cae0063dd67
Ches613 <zverkovmi@gmail.com>
2022-08-30 21:46:52 +0300

    Sem1Tast5 is done!

---
+++ Sem1Tast5/Program.cs
@@ -0,0 +1,21 @@
+﻿// =========================================================================
+// Напишите программу которая принимает число N и выводит 
+// числа от -N до N
+// =========================================================================
+
+while (true)
+{
+    Console.WriteLine("Enter number:");
+    string? inputLineN = Console.ReadLine();
+
+    if (int.TryParse(inputLineN, out int inputNumberN))
+    {
+        int startNumber = (-1) * inputNumberN;
+        while (startNumber <= inputNumberN)
+        {
+            Console.Write(startNumber + " ");
+            startNumber++;
+        }
+        return;
+    }
+}
