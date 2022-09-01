dd01439e2671ec5c940f0a36641b5a46befdfbf3
Ches613 <zverkovmi@gmail.com>
2022-09-01 19:26:24 +0300

    add RequestNumber method

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -3,21 +3,43 @@
 // а на выходе показывает все чётные числа от 1 до N.
 // =========================================================================
 
-while (true)
-{
-    Console.WriteLine("Enter number:");
-    string? inputLine = Console.ReadLine();
+const string ExitCommand = "q";
 
-    if (int.TryParse(inputLine, out int number))
+int outputNumber = 2;
+List<int> outputNumbers = new List<int>();
+
+Console.WriteLine("Enter '" + ExitCommand + "' for exit from program.");
+int number = RequestNumber();
+
+while (outputNumber <= number) // пока outputNumber <= числа введенного пользователем наполняем список четными числами.
+{
+    outputNumbers.Add(outputNumber);
+    outputNumber += 2;
+}
+
+Console.WriteLine("\n" + string.Join(", ", outputNumbers)); // превращаем элементы списка в строку с запятой в качестве разделителя и выводим в консоль
+
+
+// запрашивает число у пользователь, в качестве аргумента принимает текст запроса и команду выхода.
+int RequestNumber(string text = "Enter number", string exitCmd = "q")
+{
+
+    while (true) // код в цикле выполняется пока пользователь не введет целое число или команду выхода из программы
     {
-        int outputNumber = 2;
-        List<int> outputNumbers = new List<int>();
-        while (outputNumber <= number)
+        Console.Write(text + ": ");
+        string? inputLine = Console.ReadLine();
+
+        if (inputLine == null)
         {
-            outputNumbers.Add(outputNumber);
-            outputNumber += 2;
+            continue;
         }
-        Console.WriteLine(string.Join(", ", outputNumbers));
-        return 0;
+        if (inputLine.ToLower() == exitCmd)
+        {
+            Environment.Exit(0);
+        }
+        if (int.TryParse(inputLine, out int number))
+        {
+            return number;
+        }
     }
 }
