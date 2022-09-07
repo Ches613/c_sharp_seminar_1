9528d89c311e3b9d7755d7e7a2be7d2f0dc928f0
Zverkov Mikhail <zverkovmi@gmail.com>
2022-09-07 14:35:13 +0300

    Sem2Task16 is done!

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,34 @@
+﻿// =======================================================================================
+// Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого. 
+// =======================================================================================
+
+// чтение данных из сонсоли
+int ReadData(string line)
+{
+    Console.Write(line + ": ");
+    int number = int.Parse(Console.ReadLine()??"");
+    return number;
+}
+
+// Определяем кратность чисел
+bool SqrTest(int firstNumber, int secondNumber)
+{
+    return firstNumber == secondNumber * secondNumber;
+}
+
+// Вывод данных
+void PrintData(int firstNumber, int secondNumber)
+{
+    if (SqrTest(firstNumber, secondNumber))
+    {
+        Console.WriteLine($"Число {firstNumber} квадрат {secondNumber}");
+    }else{
+        Console.WriteLine($"Число {firstNumber} не квадрат {secondNumber}");
+    }
+}
+
+
+int num1 = ReadData("Введите первое число");
+int num2 = ReadData("Введите второе число");
+PrintData(num1, num2);
