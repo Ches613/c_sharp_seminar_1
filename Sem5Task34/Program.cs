72b462dcaee6fc56bddabe04af85bfb4d4f88236
Ches613 <zverkovmi@gmail.com>
2022-09-13 22:39:20 +0300

    Home work Task 34 of seminar 5 is done!

---
+++ Sem5Task34/Program.cs
@@ -0,0 +1,72 @@
+﻿// ====================================================================
+// Задайте массив заполненный случайными положительными трёхзначными 
+// числами. Напишите программу, которая покажет количество чётных 
+// чисел в массиве.
+// * Отсортировать массив методом пузырька
+// ====================================================================
+
+
+// Mетод генерации и заполнения  массива 
+// случайными положительными трёхзначными числами
+int[] FillThreeDigitArray(int length)
+{
+    Random rand = new Random();
+    int[] array = new int[length];
+    for (int j = 0; j < array.Length; j++)
+    {
+        array[j] = rand.Next(100, 1000);
+    }
+    return array;
+}
+
+
+// подсчитывает количество трёхзначныx чисел в массиве
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+    foreach (int item in array)
+    {
+        if (item % 2 == 0) count++;
+    }
+    return count;
+}
+
+// сортировка методом пузырька
+void BubbleSort(int[] array)
+{
+    int temp; 
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = 0; j < array.Length - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
+// вывод массив
+void Print1DArray(int[] array)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", array));
+}
+
+// вывод ответа
+void PrintAnswer(int answer)
+{
+    Console.WriteLine(
+        "Колличество трёхзначныx чётных чисел в массиве -> {0}",
+        answer
+     );
+}
+
+
+int[] array = FillThreeDigitArray(10);
+Print1DArray(array);
+PrintAnswer(CountEvenNumbers(array));
+BubbleSort(array);
+Print1DArray(array);
