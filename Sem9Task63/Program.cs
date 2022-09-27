2e60406a9f69e666a5d7dd5ecb757d024ea59061
Ches613 <zverkovmi@gmail.com>
2022-09-27 22:13:50 +0300

    Added tasks for seminar 9

---
+++ Sem9Task63/Program.cs
@@ -0,0 +1,30 @@
+﻿// ====================================================================
+// Напишите программу, выведит числа от 1 до N
+// ====================================================================
+
+
+// чтение данных из сонсоли
+int ReadData(string line)
+{
+    Console.Clear();
+    //Выводим сообщение 
+    Console.WriteLine("{0}: ", line);
+    string inputLine = Console.ReadLine() ?? "";
+    return int.Parse(inputLine);
+}
+
+// Возвращает строку чисел от 1 до N
+string LineGenRec(int numN)
+{
+    if (numN == 0) return "";
+    string outLine = LineGenRec(numN - 1) + " " + numN;
+    return outLine;
+}
+
+// Вывод в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+PrintResult(LineGenRec(ReadData("Введите число N")));
---
+++ Sem9Task69/Program.cs
@@ -0,0 +1,37 @@
+﻿// ====================================================================
+// Напишите программу, принимает A и B и возвращает а в степени ,
+// ====================================================================
+
+
+// чтение данных из сонсоли
+(int a, int b) ReadData(string line)
+{
+    Console.Clear();
+    //Выводим сообщение 
+    Console.WriteLine("{0}: ", line);
+    string inputLine = Console.ReadLine() ?? "";
+
+    // парсим полученную строку
+    string[] inputLineItems = inputLine.Split(",");
+
+    return (int.Parse(inputLineItems[0]), int.Parse(inputLineItems[1]));
+}
+
+
+// возвращает a в степени b
+double RecPow(double a, double b)
+{
+    return (b == 0) ? 1 : (b % 2 == 0) ? RecPow(a * a, b / 2) : RecPow(a, b - 1) * a;
+}
+
+
+// Вывод в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+(int a, int b) = ReadData("Введите числа А и B через запятую");
+
+DateTime d1 = DateTime.Now;
+PrintResult(RecPow(a, b).ToString());
+PrintResult((DateTime.Now -  d1).ToString());
